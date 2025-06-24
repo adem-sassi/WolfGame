0613b95a37eb47b43e69a6685d2e36f846fecbd1
azizkab <azizkabissa007@gmail.com>
2025-06-24 10:46:57 +0200

    adding the folder of the sql config (work of sandrine houbeika

---
+++ sql/wv_index.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_players_party ON players(party_id);
+CREATE INDEX idx_tours_party ON tours(party_id);
+CREATE INDEX idx_actions_tour ON actions(tour_id);
+CREATE INDEX idx_cells_party ON cells(party_id);
